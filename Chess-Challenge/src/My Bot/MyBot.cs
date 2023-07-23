d6fa9a62c01564f079ddcffc121cd7882e638bed
afonsorc <afonsorc@outlook.com>
2023-07-23 12:23:03 +0100

    refactor: change evaluations to ints

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -8,47 +8,45 @@ using System.IO;
 
 public class MyBot : IChessBot{
 
-    float[] pieceValue = { 1.0f, 3.2f, 3.4f, 5.0f, 9.5f};
+    const bool DEBUG = true;
 
-    public Move Think(Board board, Timer timer)
-    {
+    int[] pieceValue = { 100, 325, 350, 500, 900};
+    int checkmateValue = 10000;
+
+    public Move Think(Board board, Timer timer){
+
         int depth = 3;
-        Console.WriteLine((board.PlyCount + 1) / 2);
         Move bestMove = getBestMove(board, depth);
-        //Console.WriteLine("Best Move: " + bestMove);
+        if (DEBUG) Console.WriteLine((board.PlyCount + 1) / 2);
         return bestMove;
     }
 
 
-    public float evaluate(Board board){
+    public int evaluate(Board board){
 
-        float evaluation = 0.0f;
-        int whoToMove = 1;
-
-        if (!board.IsWhiteToMove) whoToMove = -1;
+        int evaluation = 0;
         for(int pl = 0; pl < 5; pl++){
             evaluation += pieceValue[pl] * board.GetAllPieceLists()[pl].Count;
             evaluation -= pieceValue[pl] * board.GetAllPieceLists()[pl + 6].Count;
         }
-        return (float)(evaluation * whoToMove);
+        return board.IsWhiteToMove ? evaluation : -evaluation;
     }
 
 
-    public float search(Board board, int depth){
+    public int search(Board board, int depth){
 
         if (depth == 0) return evaluate(board);
+        if (board.IsInCheckmate()) return -checkmateValue;
 
-        if (board.IsInCheckmate()) return -1000.0f;
+        int max = -checkmateValue - 1;
 
-        float max = -1000.0f;
-        Move[] moves = board.GetLegalMoves();
-        foreach(Move move in moves){
+        foreach(Move move in board.GetLegalMoves()){
+
             board.MakeMove(move);
-            float evaluation = -search(board, depth - 1);
+            int evaluation = -search(board, depth - 1);
             board.UndoMove(move);
-            if (evaluation > max){
-                max = evaluation;
-            }
+
+            if (evaluation > max) max = evaluation;
         }
         return max;
     }
@@ -56,23 +54,22 @@ public class MyBot : IChessBot{
 
     public Move getBestMove(Board board, int depth){
 
-        Move[] moves = board.GetLegalMoves();
-        Move bestMove = moves[0];
-        float max = -1000.0f;
+        int max = -checkmateValue - 1;
+        Move bestMove = Move.NullMove;
 
-        foreach (Move move in moves){
+        foreach (Move move in board.GetLegalMoves()){
 
             board.MakeMove(move);
-            float evaluation = -search(board, depth - 1);
+            int evaluation = -search(board, depth - 1);
             board.UndoMove(move);
 
             if (evaluation > max){
                 max = evaluation;
                 bestMove = move;
             }
-            //Console.WriteLine(move + " " + evaluation.ToString("F"));
+            if (DEBUG) Console.WriteLine(move + " " + (evaluation/100.0f).ToString("F"));
         }
-        Console.WriteLine("Best" + bestMove + " " + max.ToString("F"));
+        if(DEBUG) Console.WriteLine("Best" + bestMove + " " + (max/100.0f).ToString("F"));
         return bestMove;
     }
 }
