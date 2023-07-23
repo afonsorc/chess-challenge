4c5bb32d99a89a679604c2be2351de4d9190868b
afonsorc <afonsorc@outlook.com>
2023-07-23 01:23:21 +0100

    feat: add version 2.0.0 bot

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -5,70 +5,74 @@ using System.Numerics;
 using System.Collections.Generic;
 using System.IO;
 
+
 public class MyBot : IChessBot{
 
-    float[] pieceValue = { 1.0f, 3.0f, 3.5f, 5.0f, 9.0f, 100.0f };
+    float[] pieceValue = { 1.0f, 3.2f, 3.4f, 5.0f, 9.5f};
 
     public Move Think(Board board, Timer timer)
     {
-        float bestEval = 0.0f;
-        if (board.IsWhiteToMove)
-        {
-            bestEval = -1000.0f;
-        }
-        else
-        {
-            bestEval = 1000.0f;
-        }
+        int depth = 4;
         Console.WriteLine((board.PlyCount + 1) / 2);
-        Move[] moves = board.GetLegalMoves();
-        Move bestMove = Move.NullMove;
-        foreach (Move m in moves){
-            //Console.WriteLine(m);
-            board.MakeMove(m);
-            float newEval = GetPositionEvaluation(board);
-
-            if (board.IsInCheckmate())
-            {
-                bestMove = m;
-                break;
-            }
-
-            if (board.IsDraw())
-            {
-                newEval = 0.0f;
-            }
-            if (!board.IsWhiteToMove && bestEval < newEval){
-                bestMove = m;
-                bestEval = newEval;
-            }
-            else if (board.IsWhiteToMove && bestEval > newEval)
-            {
-                bestMove = m;
-                bestEval = newEval;
-            }
-            if(board.IsInCheck()){
-                bestMove = m;
-            }
-            //Console.WriteLine(m + " " + newEval.ToString("F"));
-            board.UndoMove(m);
-        }
-        Console.WriteLine(bestMove);
-
+        Move bestMove = getBestMove(board, depth);
+        Console.WriteLine("Best Move: " + bestMove);
         return bestMove;
     }
 
 
-    public float GetPositionEvaluation(Board board){
+    public float evaluate(Board board){
 
-        float whiteEval = 0.0f;
-        float blackEval = 0.0f;
-        for(int pl = 0; pl < 6; pl++){
-            whiteEval += pieceValue[pl] * board.GetAllPieceLists()[pl].Count;
-            //Console.WriteLine(pieceValue[pl] + " " + board.GetAllPieceLists()[pl].Count);
-            blackEval += pieceValue[pl] * board.GetAllPieceLists()[pl + 6].Count;
-            //Console.WriteLine(pieceValue[pl] + " " + board.GetAllPieceLists()[pl + 6].Count);
+        float evaluation = 0.0f;
+        int whoToMove = 1;
+
+        if (!board.IsWhiteToMove) whoToMove = -1;
+        for(int pl = 0; pl < 5; pl++){
+            evaluation += pieceValue[pl] * board.GetAllPieceLists()[pl].Count;
+            evaluation -= pieceValue[pl] * board.GetAllPieceLists()[pl + 6].Count;
         }
-        return whiteEval - blackEval;
+        return (float)(evaluation * whoToMove);
+    }
+
+
+    public float search(Board board, int depth){
+
+        if (depth == 0) return evaluate(board);
+
+        if (board.IsInCheckmate()) return -1000.0f;
+
+        float max = -1000.0f;
+        Move[] moves = board.GetLegalMoves();
+        foreach(Move move in moves){
+            board.MakeMove(move);
+            float evaluation = -search(board, depth - 1);
+            board.UndoMove(move);
+            if (evaluation > max){
+                max = evaluation;
+            }
+        }
+        return max;
+    }
+    
+
+    public Move getBestMove(Board board, int depth){
+
+        Move[] moves = board.GetLegalMoves();
+        Move bestMove = moves[0];
+        float max = -1000.0f;
+
+        foreach (Move move in moves){
+
+            board.MakeMove(move);
+            float evaluation = -search(board, depth - 1);
+            board.UndoMove(move);
+
+            if (evaluation > max){
+                max = evaluation;
+                bestMove = move;
+            }
+            Console.WriteLine(move + " " + evaluation.ToString("F"));
+        }
+        Console.WriteLine("Best" + bestMove + " " + max.ToString("F"));
+        return bestMove;
     }
 }
