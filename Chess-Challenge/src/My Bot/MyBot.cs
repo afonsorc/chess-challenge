af4812f2dc014533716615e8385398a04db9975f
afonsorc <afonsorc@outlook.com>
2023-07-24 13:21:58 +0100

    refactor: clean up functions

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,91 +1,54 @@
 ﻿using ChessChallenge.API;
 using System;
-using System.Linq;
-using System.Numerics;
-using System.Collections.Generic;
-using System.IO;
-
 
 public class MyBot : IChessBot{
 
-    const bool DEBUG = false;
-
     // Last value for initialization of moves
-    int[] pieceValue = { 100, 325, 350, 500, 900, 10000, 10001 };
+    int[] pieceValue = { 100, 320, 325, 500, 975, 10000, 32767 };
 
     public Move Think(Board board, Timer timer){
         
-        int depth = 4;
-        Move bestMove = getBestMove(board, depth);
+        int depth = 5;
+        Move bestMove = GetBestMove(board, depth);
 
         //if (DEBUG) Console.WriteLine((board.PlyCount + 1) / 2);
         return bestMove;
     }
 
 
-    public int evaluate(Board board){
+    public int Evaluate(Board board){
 
         int evaluation = 0;
-
-        for(int p = 0; p < 5; p++){
-            evaluation += pieceValue[p] * board.GetAllPieceLists()[p].Count;
-            evaluation -= pieceValue[p] * board.GetAllPieceLists()[p + 6].Count;
+        for(var p = 0; p < 5; p++){
+            evaluation += pieceValue[p] * (int)board.GetAllPieceLists()[p].Count;
+            evaluation -= pieceValue[p] * (int)board.GetAllPieceLists()[p + 6].Count;
         }
-
         return board.IsWhiteToMove ? evaluation : -evaluation;
     }
 
 
-    public int alphaBeta(Board board, int depth, int alpha, int beta){
+    public int AlphaBeta(Board board, int depth, int alpha, int beta){
 
-        if (depth == 0) return evaluate(board);
+        if (depth == 0) return Evaluate(board);
 
-        int max = -pieceValue[6];
-
-        Move[] moves = moveOrdering(board.GetLegalMoves());
-        foreach (Move move in moves){
+        foreach (Move move in MoveOrdering(board.GetLegalMoves())){
 
             board.MakeMove(move);
-            int evaluation = -alphaBeta(board, depth - 1, -beta, -alpha);
+            int evaluation = -AlphaBeta(board, depth - 1, -beta, -alpha);
             board.UndoMove(move);
 
-            // upper bound, opponent will avoid our move
+            // upper bound beta, opponent will avoid our move
+            // lower bound alpha, looking to cut worse moves
             if(evaluation >= beta) return beta;
-
-            // lower bound, looking to cut worse moves
             if(evaluation > alpha) alpha = evaluation;
         }
         return alpha;
     }
-    
-
-    public Move getBestMove(Board board, int depth){
-
-        int max = -pieceValue[6];
-        Move bestMove = Move.NullMove;
-
-        Move[] moves = moveOrdering(board.GetLegalMoves());
-        foreach (Move move in moves){
-
-            board.MakeMove(move);
-            int evaluation = -alphaBeta(board, depth - 1, -pieceValue[5], pieceValue[5]);
-            board.UndoMove(move);
-
-            if (evaluation > max){
-                max = evaluation;
-                bestMove = move;
-            }
-            if (DEBUG) Console.WriteLine(move + " " + evaluation);
-        }
-        if(DEBUG) Console.WriteLine("Best" + bestMove + " " + max);
-        return bestMove;
-    }
 
 
-    public Move[] moveOrdering(Move[] moves){
+    public Move[] MoveOrdering(Move[] moves){
 
         int[] moveScore = new int[moves.Length];
-
         for(int i = 0; i < moves.Length; i++){
             
             if(moves[i].IsPromotion && moves[i].IsCapture) moveScore[i] = 10;
@@ -99,34 +62,47 @@ public class MyBot : IChessBot{
             else if(moves[i].MovePieceType == PieceType.Pawn) moveScore[i] = 2;
             else moveScore[i] = 1;
         }
-
-        sortMoveList(moves, moveScore);
+        SortMoveList(moves, moveScore);
         return moves;
     }
 
-    public void sortMoveList(Move[] moves, int[] score){
+    public void SortMoveList(Move[] moves, int[] score){
         
         //System.Console.WriteLine("UNSORTED");
         //for(int i = 0; i < moves.Length; i++) System.Console.WriteLine(moves[i] + " " + score[i]);
-
         for(int i = 0; i < moves.Length; i++){
             int biggestValue = i;
-            for(int j = i + 1; j < moves.Length; j++){
+            for(int j = i + 1; j < moves.Length; j++)
                 if(score[j] > score[biggestValue]) biggestValue = j;
-            }
             if(biggestValue != i){
                 //System.Console.WriteLine("SWITCH " + moves[i] + " and " + moves[biggestValue]);
-                Move temp = moves[biggestValue];
-                moves[biggestValue] = moves[i];
-                moves[i] = temp;
-                int tempint = score[biggestValue];
-                score[biggestValue] = score[i];
-                score[i] = tempint;
+                (moves[i], moves[biggestValue]) = (moves[biggestValue], moves[i]);
+                (score[i], score[biggestValue]) = (score[biggestValue], score[i]);
             }
         }
-
         //System.Console.WriteLine("SORTED");
         //for(int i = 0; i < moves.Length; i++) System.Console.WriteLine(moves[i] + " " + score[i]);
         return;
+    }
+
+
+    public Move GetBestMove(Board board, int depth){
+
+        int max = -pieceValue[6];
+        Move bestMove = Move.NullMove;
+
+        foreach (Move move in MoveOrdering(board.GetLegalMoves())){
+
+            board.MakeMove(move);
+            int evaluation = -AlphaBeta(board, depth - 1, -pieceValue[5], pieceValue[5]);
+            board.UndoMove(move);
+
+            if (evaluation > max){
+                max = evaluation;
+                bestMove = move;
+            }
+            //Console.WriteLine(move + " " + evaluation);
+        }
+        return bestMove;
     }
 }
