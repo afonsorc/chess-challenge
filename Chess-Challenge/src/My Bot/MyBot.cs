f9a62cc4c713788f2d008798cafcf884c31d60cc
afonsorc <afonsorc@outlook.com>
2023-07-30 04:25:09 +0100

    feat: add pesto psts and tapered evaluation

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -1,6 +1,5 @@
 ﻿using ChessChallenge.API;
 using System;
-using System.Diagnostics;
 
 
 public class MyBot : IChessBot    {
@@ -18,130 +17,59 @@ public class MyBot : IChessBot    {
             }
         }
 
-        const bool TEST_DEPTH = true;
-        const int TIME_PER_MOVE = 2000;
-
-        const bool TIMER = !TEST_DEPTH;
-        const int MAX_DEPTH = TEST_DEPTH ? 8 : 20; 
+        Move bestRootMove = Move.NullMove;
         const int ttSize = 8388608;
         public Entry[] transpositionTable = new Entry[ttSize];
-        int nNodes = 0;
 
         // PeSTO Piece Square Table
+        readonly static ulong[] pestoPST = {657614902731556116, 420894446315227099, 384592972471695068, 312245244820264086, 364876803783607569, 366006824779723922, 366006826859316500, 786039115310605588, 421220596516513823, 366011295806342421, 366006826859316436, 366006896669578452, 162218943720801556, 440575073001255824, 657087419459913430, 402634039558223453, 347425219986941203, 365698755348489557, 311382605788951956, 147850316371514514, 329107007234708689, 402598430990222677, 402611905376114006, 329415149680141460, 257053881053295759, 291134268204721362, 492947507967247313, 367159395376767958, 384021229732455700, 384307098409076181, 402035762391246293, 328847661003244824, 365712019230110867, 366002427738801364, 384307168185238804, 347996828560606484, 329692156834174227, 365439338182165780, 386018218798040211, 456959123538409047, 347157285952386452, 365711880701965780, 365997890021704981, 221896035722130452, 384289231362147538, 384307167128540502, 366006826859320596, 366006826876093716, 366002360093332756, 366006824694793492, 347992428333053139, 457508666683233428, 329723156783776785, 329401687190893908, 366002356855326100, 366288301819245844, 329978030930875600, 420621693221156179, 422042614449657239, 384602117564867863, 419505151144195476, 366274972473194070, 329406075454444949, 275354286769374224, 366855645423297932, 329991151972070674, 311105941360174354, 256772197720318995, 365993560693875923, 258219435335676691, 383730812414424149, 384601907111998612, 401758895947998613, 420612834953622999, 402607438610388375, 329978099633296596, 67159620133902};
         readonly int[] pieceValue = { 0, 100, 310, 330, 500, 1000, 0 };
-        readonly ulong[,] setPST = {{0, 3617008641903833650, 723412809732590090, 361706447983740165, 86234890240, 358869600189152005, 363113628838791685, 0},{14832572258041583566, 15558810812658215896, 16285027312364814306, 16286440206365557986, 16285032831482003426, 16286434687248368866, 14832572258041583566, 15558810834216938456},{17002766404949505516, 17726168133330272246, 17726173674006183926, 17727581048889738486, 17726179171564650486, 17728993921331759606, 17727575508213826806, 17002766404949505516},{0, 363113758191127045, 18086456103519911931, 18086456103519911931, 18086456103519911931, 18086456103519911931, 18086456103519911931, 21558722560},{17002766426508228076, 17726168133330272246, 17726173652447461366, 18086461622637101051, 18086461622637101056, 17726173652447462646, 17726168133330599926, 17002766426508228076},{16273713057448318946, 16273713057448318946, 16273713057448318946, 16273713057448318946, 16997114785829085676, 17720516557327297526, 1446781380292776980, 1449607125176819220}};
-        readonly ulong[,] pestoMiddleGame;
-        readonly ulong[,] pestoEndGame;
-        Move bestRootMove = Move.NullMove;
+        readonly int[] piecePhase = { 0, 0, 1, 1, 2, 4, 0 }; 
+
 
         public Move Think(Board board, Timer timer){
-            
-            // depth should be even to end on opponent's turn 
-            nNodes = 0;
 
-            var sw = new Stopwatch();
-            //Console.WriteLine((board.PlyCount + 1) / 2);
-            int evaluation;
-            int alpha = -20000;
-            int beta = 20000;
-            int window = 100;
-
-            for(int depth = 1; depth <= MAX_DEPTH; depth++){
-                sw.Start();
-                evaluation = AlphaBetaSearch(board, depth, alpha, beta, timer, 0);
-                sw.Stop();
-
-                long ticks = sw.ElapsedTicks;
-                int ms = (int)(1000.0 * ticks / Stopwatch.Frequency);
-                System.Console.WriteLine("info depth " + depth + " nodes " + nNodes + " time " + ms + " move " + bestRootMove);
-/*
-                if(evaluation <= alpha || evaluation >= beta){
-                    if(evaluation <= alpha) alpha -= 100;
-                    if(evaluation >= beta) beta += 100;
-                    depth--;
-                    System.Console.WriteLine("REPEAT SEARCH");
-                }else{
-                    alpha = evaluation - window;
-                    beta = evaluation + window;
-                    System.Console.WriteLine("WORKS");
-                }*/
-
-                if(timer.MillisecondsElapsedThisTurn >= timer.MillisecondsRemaining / TIME_PER_MOVE && TIMER)
-                    break;
+            for(int depth = 1; depth <= 20; depth++){
+                int evaluation = AlphaBetaSearch(board, depth, -20000, 20000, timer, 0);
+                if(timer.MillisecondsElapsedThisTurn >= timer.MillisecondsRemaining / 30) break;
             }
-
-            //System.Console.WriteLine("Stored in TT: " + nTranspositions);
-            //System.Console.WriteLine("Found Transposition: " + nLookups);
-
-            //Console.WriteLine(bestMove);
-            //Console.WriteLine(move + " " + evaluation);
-    /*
-            ulong[,] test = new ulong[6, 8];
-            for(int rank = 0; rank < 8; rank++){
-                for(int file = 0; file < 8; file++){
-                    test[0, rank] += (ulong)pstP[rank, file] << (8 * file);
-                    test[1, rank] += (ulong)pstN[rank, file] << (8 * file);
-                    test[2, rank] += (ulong)pstB[rank, file] << (8 * file);
-                    test[3, rank] += (ulong)pstR[rank, file] << (8 * file);
-                    test[4, rank] += (ulong)pstQ[rank, file] << (8 * file);
-                    test[5, rank] += (ulong)pstK[rank, file] << (8 * file);
-                }
-            }
-
-            for(int i = 0; i < 6; i++){
-                for(int j = 0; j < 8; j++)
-                    System.Console.Write(test[i,j] + ", ");
-                System.Console.WriteLine();
-            }*/
-
-
-
-
             return bestRootMove == Move.NullMove ? board.GetLegalMoves()[0] : bestRootMove;
         }
 
 
-        public sbyte GetValuePST(ulong[,] bitboard, int piece, int index){
-            return (sbyte)((bitboard[piece, index / 8] >> (56 - (8 * (index % 8)))) & (1 << 8) - 1);;
+        public int GetValuePesto(int index){
+            return (int)(((pestoPST[index / 10] >> (6 * (index % 10))) & 63) - 20) * 8;
         }
+
 
         public int Evaluate(Board board){
 
-            int evaluation = 0;
-            for(var p = 1; p < 6; p++){
-                evaluation += pieceValue[p] * (int)board.GetAllPieceLists()[p - 1].Count;
-                evaluation -= pieceValue[p] * (int)board.GetAllPieceLists()[p + 5].Count;
-            }
+            int phase = 0;
+            int[] midgame = new int[2];
+            int[] endgame = new int[2];
+            bool isWhite = true;
 
-            for(var piece = PieceType.Pawn; piece <= PieceType.King; piece++){
-                ulong white = board.GetPieceBitboard(piece, true);
-                while(white != 0){
-                    evaluation += GetValuePST(setPST, (int)piece - 1, BitboardHelper.ClearAndGetIndexOfLSB(ref white) ^ 56);
+            for(int side = 0; side < 2; side ++){
+                for(var piece = PieceType.Pawn; piece <= PieceType.King; piece++){
+                    ulong bitboard = board.GetPieceBitboard(piece, isWhite);
+                    int materialEvaluation = pieceValue[(int)piece] * board.GetPieceList(piece, isWhite).Count;
+                    midgame[side] += materialEvaluation;
+                    endgame[side] += materialEvaluation;
+                    phase += board.GetPieceList(piece, isWhite).Count * piecePhase[(int)piece];                
+                    while(bitboard != 0){
+                        int index = 128 * ((int)piece - 1) + BitboardHelper.ClearAndGetIndexOfLSB(ref bitboard) ^ (isWhite ? 56 : 0);
+                        midgame[side] += GetValuePesto(index);
+                        endgame[side] += GetValuePesto(index + 64);
+                    }
                 }
-                ulong black = board.GetPieceBitboard(piece, false);
-                while(black != 0){
-                   evaluation -= GetValuePST(setPST, (int)piece - 1, BitboardHelper.ClearAndGetIndexOfLSB(ref black) ^ 0);
-                }
+                isWhite = false;
             }
 
-    /*
-            if(!board.HasKingsideCastleRight(true) && !board.HasQueensideCastleRight(true)){
-                evaluation -= 150;
-            }
-
-            if(!board.HasKingsideCastleRight(false) && !board.HasQueensideCastleRight(false)){
-                evaluation += 150;
-            }*/
-            
-            //System.Console.WriteLine("---" + evaluation);
-
-            //if(!board.HasKingsideCastleRight(true)) evaluation -= 50;
-            //if(!board.HasQueensideCastleRight(true)) evaluation -= 20;
-            //if(!board.HasKingsideCastleRight(false)) evaluation += 50;
-            //if(!board.HasQueensideCastleRight(false)) evaluation += 20;
-            //System.Console.WriteLine(evaluation);
-            return board.IsWhiteToMove ? evaluation : -evaluation;
+            phase = Math.Min(phase, 24);
+            int evaluation = ((midgame[0] - midgame[1]) * phase + (endgame[0] - endgame[1]) * (24 - phase)) / 24;
+            return board.IsWhiteToMove ? evaluation + 10 : -10 -evaluation;
         }
+
 
         public void OrderMoves(Move[] moves, Entry entry){
 
@@ -170,10 +98,10 @@ public class MyBot : IChessBot    {
             }
         }
 
+
         public int AlphaBetaSearch(Board board, int depth, int alpha, int beta, Timer timer, int ply){
 
             bool isRoot = ply <= 0;
-            //System.Console.WriteLine("PLY " + ply);
 
             // repetition occurs more often so we check it first
             // might implement penalty for draw or contempt factor
@@ -187,56 +115,46 @@ public class MyBot : IChessBot    {
             if(!isRoot && entry.key == zobristHash && entry.depth >= depth)
                     if(entry.evaluationType == 0 || (entry.evaluationType == 1 && entry.evaluation <= alpha) || (entry.evaluationType == 2 && entry.evaluation >= beta))
                         return entry.evaluation;
+            
 
-
-            int alphaStart = alpha;
             bool isQueiescenceSearch = depth <= 0;
             int max = -30000;
+            int alphaStart = alpha;
 
             if(isQueiescenceSearch){
                 max = Evaluate(board);
-                //System.Console.WriteLine(max);
-                if(max >= beta) return max;
-                //int delta = 100;
-                //if(standPat < alpha - delta) return alpha;
+                if(max >= beta) return beta;
                 if(max > alpha) alpha = max;
             }
-
-            //Check for checkmate or stalemate
-            if(board.GetLegalMoves().Length == 0) return board.IsInCheck() ? -10000 : 0;
 
 
             Move[] moves = board.GetLegalMoves(isQueiescenceSearch);
             OrderMoves(moves, entry);
 
-            int evaluationType = 0;
             Move bestMove = Move.NullMove;
             foreach(Move move in moves){
 
-                if(timer.MillisecondsElapsedThisTurn >= timer.MillisecondsRemaining / TIME_PER_MOVE && TIMER) return 10000;
+                if(timer.MillisecondsElapsedThisTurn >= timer.MillisecondsRemaining / 30) return 10000;
 
                 board.MakeMove(move);
-                nNodes++;
                 int evaluation = -AlphaBetaSearch(board, depth - 1, -beta, -alpha, timer, ply + 1);
-                //for(int i = 0; i < ply; i++) System.Console.Write(" ");
-                //System.Console.WriteLine(depth + ". " + move + " " + evaluation + " | A: " + alpha + "B: " + beta);
-                if(isRoot) System.Console.WriteLine(move + " " + evaluation);
                 board.UndoMove(move);
 
                 if(evaluation > max){
                     max = evaluation;
                     bestMove = move;
                     if(isRoot) bestRootMove = move;
-
                     alpha = Math.Max(alpha, evaluation);
                     if(alpha >= beta) break;
                 }
             }
+
+            //Check for checkmate or stalemate
+            if(board.GetLegalMoves().Length == 0) return board.IsInCheck() ? -10000 : 0;
                       
+            int evaluationType = 0;
             if(max <= alphaStart) evaluationType = 1;
             else if(max >= beta) evaluationType = 2;
-
-            //if(isRoot) System.Console.WriteLine("Best " + bestRootMove + " " + max);
 
             transpositionTable[zobristHash % ttSize] = new Entry(zobristHash, bestMove, depth, alpha, evaluationType);
             return max;
